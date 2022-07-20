f25d92dde2d06e6ef502d2a5369712b5fa505cbf
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-20 23:52:40 +0600

    added animation with lottie file in the welcome page

--- delivering_app/lib/view/loginAndSignUp/WelcomePage.dart
+++ delivering_app/lib/view/loginAndSignUp/WelcomePage.dart
@@ -5,62 +5,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
+import '../../widgets/lottie fles/my_lottie.dart';
+
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(       
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 26),
-              child: SvgPicture.asset(
-              "images/delivery.svg",
-              width: 170,
-              height: 150,
+          MyLottie(),
+          // Container(
+          //   color: Colors.white,
+          //   child: Padding(
+          //     padding: const EdgeInsets.symmetric(horizontal: 26),
+          //     child: SvgPicture.asset(
+          //     "images/delivery.svg",
+          //     width: 170,
+          //     height: 150,
+          //     ),
+          //   ),
+          // ),
+          Text(
+            'Welcome',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: primaryColour,
+            ),
+            onPressed: () {
+              Get.to(() => LoginForm());
+            },
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
             ),
           ),
-          Text('Welcome', style: TextStyle(fontSize: 40,
-          fontWeight: FontWeight.bold),),
           ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: primaryColour,
-
-                ),
-                onPressed: () {
-                  Get.to(() => LoginForm());
-                },
-                child: const Text(
-                  "Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
+            style: ElevatedButton.styleFrom(
+              primary: primaryColour,
+            ),
+            onPressed: () {
+              Get.to(() => SignUPage());
+            },
+            child: const Text(
+              "Signup",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: primaryColour,
-
-                ),
-                onPressed: () {
-                  Get.to(() => SignUPage());
-                },
-                child: const Text(
-                  "Signup",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
+            ),
+          ),
         ],
       ),
-      );
+    );
   }
 }
