60df4fa25e737beddd4c19ca76ab75745ba77e62
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-20 22:48:06 +0600

    adding card swiper to the app

---
+++ delivering_app/lib/models/card_swiper_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+final List<String> sliderItems = [
+  elevenEleven,
+  blackFriday,
+  cyberMonday,
+  xmas,
+];
+String elevenEleven = 'images/order.jpg';
+String blackFriday = 'images/del1.jpg';
+String cyberMonday = 'images/del2.jpg';
+String xmas = 'images/discount.jpg';
+
+class CardSwiperModel extends StatelessWidget {
+  final String image;
+  final BoxFit fit;
+  final double width, height;
+  const CardSwiperModel(this.image,
+      {Key? key, required this.fit, required this.height, required this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      fit: fit,
+      width: width,
+      height: height,
+    );
+  }
+}
