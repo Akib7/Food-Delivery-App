55436343fbc88599f5725e11476102dd907dfad2
Akib Ahmed <akibahmed7360@gmail.com>
2022-08-15 16:18:01 +0600

    succesfully added multiple actors to the project

--- delivering_app/lib/controller/authController.dart
+++ delivering_app/lib/controller/authController.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:delivering_app/Constants/firebase_auth_constants.dart';
 
 import 'package:delivering_app/helper/firestore_db.dart';
@@ -7,47 +8,38 @@ import 'package:delivering_app/view/loginAndSignUp/WelcomePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
+import '../view/food items/Burger.dart';
+
 class AuthController extends GetxController {
   // UserProvider userProvider = Provider.of<UserProvider>(context);
   late Rx<User?> firebaseUser;
   static AuthController instance = Get.find<AuthController>();
 
-  // void getUserType() async {
-  //   QuerySnapshot type = await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .collection('typeOfUser')
-  //       .get();
+  var typeOfUser;
+  getUserData() async {
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    final userData = userDoc.data()! as dynamic;
+    print(userData);
+    // String name = userData['name'];
+    typeOfUser = userData['typeOfUser'];
+    print(typeOfUser);
+  }
+
+  // getData() {
+  //   FirebaseAuth.instance.authStateChanges().listen((User? user) {
+  //     if (user != null) {
+  //       print(user.metadata);
+  //     }
+  //   });
   // }
-
-  // late UserModel currentData;
-
-  // void getUserData() async {
-  //   UserModel userModel;
-  //   var value = await FirebaseFirestore.instance
-  //       .collection("users")
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .get();
-  //   if (value.exists) {
-  //     userModel = UserModel(
-  //       email: value.get("email"),
-  //       // userImage: value.get("userImage"),
-  //       userName: value.get("userName"), typeOfUser: value.get("typeOfUser"),
-  //       // userUid: value.get("userUid"),
-  //     );
-  //     currentData = userModel;
-  //   }
-  // }
-
-  // UserModel get currentUserData {
-  //   return currentData;
-  // }
-
-  String? user = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   void onReady() {
     super.onReady();
+    getUserData();
     firebaseUser = Rx<User?>(auth.currentUser);
     firebaseUser.bindStream(auth.userChanges());
     ever(firebaseUser, _setIntializeScreen);
@@ -56,15 +48,15 @@ class AuthController extends GetxController {
   _setIntializeScreen(User? user) {
     if (user == null) {
       Get.offAll(() => WelcomePage());
-    } else {
+    } else if (typeOfUser == 'Admin') {
       // if (currentUserData.typeOfUser == 'Admin')
       Get.offAll(() => MyHomePage(
             onTap: () {},
           ));
+      // getUserData();
+    } else if (typeOfUser == 'Customer') {
+      Get.offAll(() => Burger());
     }
-    // else if (currentUserData.typeOfUser == 'Customer') {
-    //   Get.offAll(() => Burger());
-    // }
   }
 
   void signup(
